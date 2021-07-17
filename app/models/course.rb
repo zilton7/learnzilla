d7e800ed7c86fa0cf7ee1bf14493fc7e406491c7
Zil Norvilis <zilasino27@gmail.com>
2021-07-17 12:20:49 +0200

    Add user to course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { minimum:5 }
   has_rich_text :description
+
+  belongs_to :user
   
   def to_s
     title
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-Course.destroy_all
+User.destroy_all
+user = User.create!(email: 'admin@mail.com', password: '123456', password_confirmation: '123456')
 
+Course.destroy_all
 30.times do
   Course.create!(
+    user: user,
     title: Faker::Educator.course_name,
     description: Faker::TvShows::MichaelScott.quote
   )
